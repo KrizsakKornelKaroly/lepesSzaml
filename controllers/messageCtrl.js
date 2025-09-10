6dae1b94849d6da28146efc8c83e2bb86098c56a
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-09-10 08:59:30 +0200

    alert áthelyezés

---
+++ controllers/messageCtrl.js
@@ -0,0 +1,21 @@
+function regAlerts(message, type){
+    let alertPlaceholder = document.querySelector('#liveAlertPlaceholder')
+    let alert = document.createElement('div')
+    alert.classList.add('alert', `alert-${type}`, 'alert-dismissible', 'fade', 'show')
+
+    let alertBody = document.createElement('div')
+    alertBody.innerHTML = message;
+
+    let alertClose = document.createElement('button')
+    alertClose.classList.add('btn-close')
+    alertClose.dataset.bsDismiss = "alert"
+
+    alert.appendChild(alertBody)
+    alert.appendChild(alertClose)
+
+    alertPlaceholder.appendChild(alert)
+
+    setTimeout(() => {
+        alert.remove();
+    }, 5000);
+}
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,32 +2,6 @@ const passwdRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-
-
-//Bővíteni bootstrap alertekkel + timerrel eltűnik (3sec)
-
-function regAlerts(message, type){
-    let alertPlaceholder = document.querySelector('#liveAlertPlaceholder')
-    let alert = document.createElement('div')
-    alert.classList.add('alert', `alert-${type}`, 'alert-dismissible', 'fade', 'show')
-
-    let alertBody = document.createElement('div')
-    alertBody.innerHTML = message;
-
-    let alertClose = document.createElement('button')
-    alertClose.classList.add('btn-close')
-    alertClose.dataset.bsDismiss = "alert"
-
-    alert.appendChild(alertBody)
-    alert.appendChild(alertClose)
-
-    alertPlaceholder.appendChild(alert)
-
-    setTimeout(() => {
-        alert.remove();
-    }, 5000);
-}
-
 async function registration() {
     /*await fetch('http://localhost:3000/users')
     .then(res => res.json())
@@ -70,7 +44,6 @@ async function registration() {
                     }
                 )
         });
-        //console.log("Status: ", res.status)
         let alertStatus = res.status == 200 ? 'success' : 'danger'; 
         const data = await res.json();
         regAlerts(`${data.msg}`, alertStatus);
